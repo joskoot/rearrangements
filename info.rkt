5518888c98891c78db036e2014f4db80cde1ad09
Jos Koot <jos.koot@gmail.com>
2021-11-26 17:24:31 +0100

    Create info.rkt

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang setup/infotab
+(define scribblings (list (list "rearrangements.scrbl")))
+(define name "planet-fmt")
+(define blurb (list "rearrangements"))
+(define primary-file "rearrangements.rkt")
+(define categories (list 'rearrangements))
